0e92828e6a31d6f434599cd35dbf24090843a8f7
Tiago Oliveira Weber <tiago.oliveira.weber@gmail.com>
2018-04-17 00:26:54 -0300

    update

--- blog/attachments/ob-spice.el
+++ blog/attachments/ob-spice.el
@@ -1,8 +1,8 @@
 ;;; ob-spice.el --- org-babel functions for spice evaluation
 
 ;; Author: Tiago Oliveira Weber
-;; Version: 0.4
-;; 2016
+;; Version: 0.5
+;; 2016-2018
 ;; Homepage: http://tiagoweber.github.io
 
 ;; Org-Babel support for evaluating spice script.
@@ -10,6 +10,10 @@
 
 ;;; Requirements:
 ;; ngspice
+
+
+;; Updates from version 0.4
+;;    use org-babel--get-vars params instead of org-babel-get-header (which is no longer present in org-mode)
 
 ;;; Code:
 (require 'ob)
@@ -30,7 +34,9 @@
 (defun org-babel-expand-body:spice (body params)
   "Expand BODY according to PARAMS, return the expanded body."
   (let* (
-	 (vars (mapcar #'cdr (org-babel-get-header params :var)))
+	 ;;(vars (mapcar #'cdr (org-babel-get-header params :var)))
+	 ;;(vars (mapcar #'cdr (org-babel--get-vars params)))
+	 (vars (org-babel--get-vars params))
 	 )
 
     (setq newbody "");
@@ -51,12 +57,12 @@
 													   (lambda (key candidate)
 													     (string= key candidate))))))
 
-									   (if (not (eq newword nil))
+									  (if (not (eq newword nil))
 									       (if (not (stringp newword))
 										   (setq word (number-to-string newword))
 										 (setq word newword)
 										 ))
-									   )							     
+									   )
 								      ) ;; end of (if (string-match "\\$\\(.*\\)\\[\\(.*\\)\\]" word))
 								    (if (string-match "\\$\\(.*\\)\\." word) ;; if variable has a dot in the end
 									(progn
@@ -108,15 +114,27 @@
 (defun org-babel-execute:spice (body params)
   "Execute a block of Spice code with org-babel."
   (let ((body (org-babel-expand-body:spice body params))
-	(vars (mapcar #'cdr (org-babel-get-header params :var)))	
-	)    
+	;;(vars (mapcar #'cdr (org-babel-get-header params :var)))
+
+	;;(vars (mapcar #'cdr (org-babel--get-vars params)))
+	(vars
+	 ;(mapcar #'cdr
+	         (org-babel--get-vars params)
+	 ;	 )
+	 )
+
+	
+	)
+    
+    ;;(isoctave (cdr (car (org-babel-get-header params :isoctave))))
+	
 
     ;;******************************
-    ;; clean temporary files
+    ;; CLEAN temporary files
     (mapc (lambda (pair)
 	    (when (string= (car pair) "file")
 	      (setq textfile (concat (cdr pair) ".txt"))
-	      (setq imagefile (concat (cdr pair) ".png"))	      
+	      (setq imagefile (concat (cdr pair) ".png"))
 	      )
 	    )
 	  vars)
@@ -124,8 +142,24 @@
 ;;    (if (file-readable-p imagefile)    (delete-file imagefile))
     ;;*******************************
 
-    (org-babel-eval "ngspice -b " body)
-
+    (if 1;;(eq isoctave nil)
+	(let ((exec_string "ngspice -b "))
+	  ;;(org-babel-eval "ngspice -b " body)
+	  (org-babel-eval exec_string body)
+	  (message (concat exec_string body))
+	  )
+      (let ((exec_string "ngspice_octave tempfile"))
+	;;(org-babel-eval "ngspice -b " body)
+	(with-temp-file "tempfile"
+	  (progn
+	    (insert body)
+	    )
+	  )
+	(org-babel-eval exec_string "")
+	(message exec_string)
+	)
+      ) ;; end of if
+    
     ;; loop through all pairs (elements) of the list vars and set text and image file if finds "file" var
     (mapc (lambda (pair)
 	    (when (string= (car pair) "file")
@@ -134,7 +168,9 @@
 	      )
 	    )
 	  vars)
-    ;; produce results        
+    ;; produce results
+    (makunbound 'result)
+    
     ;; THE FOLLOWING WAS COMMENTED TEMPORARILY
     ;; (concat
     ;;  (if (file-readable-p textfile)
@@ -156,8 +192,10 @@
     (if (file-readable-p imagefile)
 	(progn
 	  ;; test if result exist already
-	  ;;(if (boundp 'result)
+	  (if (boundp 'result)
 	      (add-to-list 'result (concat '"[[file:./" imagefile "]]") t)    ;; add imagefile to last entry
+	    (setq result (concat '"[[file:./" imagefile "]]")) ;; else
+	    )
 	    ;;(concat '"[[file:./" imagefile "]]")
 	    ;;)  
 	  )
